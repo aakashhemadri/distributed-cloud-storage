b420dd1d4bea8652321d5298f776602716aebb9e
Aakash Hemadri <aakashhemadri123@gmail.com>
2020-02-02 19:38:30 +0530

    Final

--- balancer/balance.py
+++ balancer/balance.py
@@ -1,5 +1,8 @@
 from config import config
 
+node_config = []
+
 def balance():
+    for i in range(config['nodes_count'])
     return int(1)
     
--- balancer/node.py
+++ balancer/node.py
@@ -2,9 +2,11 @@ from flask import redirect
 from flask_restful import Resource
 from balance import balance
 
+url_ = "http://127.0.0.1:500"
+
 class Node(Resource):
     def get():
-        node = balance()
+        nodes = balance() #[(,),(,),]
         location = "http://127.0.0.1:500" + str(node) + request.script_root + request.path
         resp = requests.get(location)
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
@@ -12,7 +14,7 @@ class Node(Resource):
         response = Response(resp.content, resp.status_code, headers)
         return response
     def put():
-        node = balance()
+        nodes = balance()
         location = "http://127.0.0.1:500" + str(node) + request.script_root + request.path
         resp = requests.put(location,json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
@@ -20,7 +22,7 @@ class Node(Resource):
         response = Response(resp.content, resp.status_code, headers)
         return response
     def post():
-        node = balance()
+        nodes = balance()
         location = "http://127.0.0.1:500" + str(node) + request.script_root + request.path
         resp = requests.post(location,json=request.get_json())
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
@@ -28,7 +30,7 @@ class Node(Resource):
         response = Response(resp.content, resp.status_code, headers)
         return response
     def delete():
-        node = balance()
+        nodes = balance()
         location = "http://127.0.0.1:500" + str(node) + request.script_root + request.path
         resp = requests.delete(location).content
         response = Response(resp.content, resp.status_code, headers)
